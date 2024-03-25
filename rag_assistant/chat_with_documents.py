e76728f4fc032e552507ef798d7d729dbe1dffae
Pierre Bittner <pierre@bittner.fr>
2024-03-25 23:22:34 +0100

    Ajout de conversation starter et d'un system prompt

--- rag_assistant/chat_with_documents.py
+++ rag_assistant/chat_with_documents.py
@@ -11,7 +11,8 @@ from langchain.callbacks.base import BaseCallbackHandler
 from langchain.chains import ConversationalRetrievalChain
 from langchain.vectorstores import DocArrayInMemorySearch
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import Chroma
+#from langchain.vectorstores import Chroma
+from langchain_community.vectorstores import Chroma
 from langchain.embeddings.openai import OpenAIEmbeddings
 from dotenv import load_dotenv, find_dotenv
 
@@ -23,11 +24,47 @@ st.title("Finaxys: Chat with Documents")
 
 # Define paths for PDF files
 pdf_files_paths = [
-    "data/sources/pdf/arxiv/2210.01241.pdf",
+    # "data/sources/pdf/arxiv/2210.01241.pdf",
     # "data/sources/aws/waf/The_6_Pillars_of_the_AWS_Well-Architected_Framework.md",
-    "data/sources/pdf/aws/waf/AWS_Well-Architected_Framework.pdf"
+    "data/sources/pdf/aws/waf/AWS_Well-Architected_Framework.pdf",
+    "data/sources/pdf/Questionnaire d'évaluation des risques applicatifs pour le Cloud Public.pdf",
+    # "data/sources/pdf/owasp/LLM_AI_Security_and_Governance_Checklist-v1_FR.pdf",
+    "data/sources/pdf/enisa/Cloud Security Guide for SMEs.pdf",
+    "data/sources/pdf/aws/caf/aws-caf-for-ai.pdf",
     # Add more paths as needed
 ]
+
+__template2__ = """You are an assistant designed to guide users through a structured risk assessment questionnaire for cloud deployment. 
+    The questionnaire is designed to cover various pillars essential for cloud architecture,
+     including security, compliance, availability, access methods, data storage, processing, performance efficiency,
+      cost optimization, and operational excellence.
+
+    For each question, you are to follow the "Chain of Thought" process. This means that for each user's response, you will:
+
+    - Acknowledge the response,
+    - Reflect on the implications of the choice,
+    - Identify any risks associated with the selected option,
+    - Suggest best practices and architecture patterns that align with the user’s selection,
+    - Guide them to the next relevant question based on their previous answers.
+
+    Your objective is to ensure that by the end of the questionnaire, the user has a clear understanding of the appropriate architecture and services needed for a secure, efficient, and compliant cloud deployment. Remember to provide answers in a simple, interactive, and concise manner.
+
+    Process:
+
+    1. Begin by introducing the purpose of the assessment and ask the first question regarding data security and compliance.
+    2. Based on the response, discuss the chosen level of data security, note any specific risks or requirements, and recommend corresponding cloud services or architectural patterns.
+    3. Proceed to the next question on application availability. Once the user responds, reflect on the suitability of their choice for their application's criticality and suggest availability configurations.
+    4. For questions on access methods and data storage, provide insights on securing application access points or optimizing data storage solutions.
+    5. When discussing performance efficiency, highlight the trade-offs between performance and cost, and advise on scaling strategies.
+    6. In the cost optimization section, engage in a brief discussion on budgetary constraints and recommend cost-effective cloud resource management.
+    7. Conclude with operational excellence, focusing on automation and monitoring, and propose solutions for continuous integration and deployment.
+    8. After the final question, summarize the user's choices and their implications for cloud architecture.
+    9. Offer a brief closing statement that reassures the user of the assistance provided and the readiness of their cloud deployment strategy.
+
+    Keep the interactions focused on architectural decisions without diverting to other unrelated topics. 
+    You are not to perform tasks outside the scope of the questionnaire, 
+    such as executing code or accessing external databases. 
+    Your guidance should be solely based on the information provided by the user in the context of the questionnaire."""
 
 
 @st.cache_resource(ttl="1h")
@@ -58,15 +95,19 @@ def configure_retriever(pdf_files_paths):
 
 
 class StreamHandler(BaseCallbackHandler):
-    def __init__(self, container: st.delta_generator.DeltaGenerator, initial_text: str = ""):
+    def __init__(self, container: st.delta_generator.DeltaGenerator, initial_text: str = "", initial_system_prompt: str = ""):
         self.container = container
         self.text = initial_text
+        self.initial_system_prompt = initial_system_prompt
         self.run_id_ignore_token = None
 
     def on_llm_start(self, serialized: dict, prompts: list, **kwargs):
         # Workaround to prevent showing the rephrased question as output
         if prompts[0].startswith("Human"):
             self.run_id_ignore_token = kwargs.get("run_id")
+            user_prompt = prompts[0]  # La requête de l'utilisateur
+            combined_prompt = f"{self.initial_system_prompt}\n\n{user_prompt}"
+            prompts[0] = combined_prompt  # Mettre à jour la requête avec la combinaison
 
     def on_llm_new_token(self, token: str, **kwargs) -> None:
         if self.run_id_ignore_token == kwargs.get("run_id", False):
@@ -112,16 +153,22 @@ qa_chain = ConversationalRetrievalChain.from_llm(
 #     msgs.add_ai_message("How can I help you?")
 
 # Define suggested questions
+# suggested_questions = [
+#     "What is reinforcement learning?",
+#     "What is reinforcement learning for human feedback?",
+#     "How to process reinforcement learning?",
+#     "What are the advantages of reinforcement learning?"
+# ]
 suggested_questions = [
-    "What is reinforcement learning?",
-    "What is reinforcement learning for human feedback?",
-    "How to process reinforcement learning?",
-    "What are the advantages of reinforcement learning?"
+    "Comment sécuriser les données sensibles ?",
+    "Quelles stratégies pour une haute disponibilité ?",
+    "Quels sont les mécanismes d'authentification API ?",
+    "Comment assurez l'efficacité des performances ?",
 ]
 
 # Display "How can I help you?" message followed by suggested questions
 with st.chat_message("assistant"):
-    st.write("How can I help you?")
+    st.write("Comment puis-je vous aider?")
 
 for i, question in enumerate(suggested_questions, start=1):
     if st.button(f"{question}", key=f"suggested_question_{i}"):
@@ -142,5 +189,5 @@ if user_query := st.chat_input(placeholder="Ask me anything!"):
 
     with st.chat_message("assistant"):
         retrieval_handler = PrintRetrievalHandler(st.container())
-        stream_handler = StreamHandler(st.empty())
+        stream_handler = StreamHandler(st.empty(), initial_system_prompt=__template2__)
         response = qa_chain.run(user_query, callbacks=[retrieval_handler, stream_handler])
