78d0f15aede782bbaaa0851271ea8f1f73976336
Pierre Bittner <pierre@bittner.fr>
2024-06-18 18:11:32 +0200

    fichier utilitaire pour vérifier un simple password (pas très sécurisé).

---
+++ rag_assistant/utils/auth.py
@@ -0,0 +1,26 @@
+import hmac
+import streamlit as st
+
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the passward is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
