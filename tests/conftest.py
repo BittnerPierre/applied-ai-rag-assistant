a34df506b1f8f5f0adcb90399a9522c6a1279c3c
Pierre Bittner <pierre@bittner.fr>
2024-06-19 11:04:03 +0200

    Reinitialisation de la base trulens au niveau de la session de test et non plus package

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,5 +2,19 @@
 import sys
 import os
 
+import pytest
+
+from trulens_eval import (
+    Tru
+)
+
+
 # Add the root directory of the project to the Python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
+@pytest.fixture(scope="session")
+def trulens_prepare():
+    tru = Tru(database_redact_keys=True)
+    tru.reset_database()
+    return tru
